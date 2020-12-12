f6316519a609b32b40109c9c35467cdf04479d24
amanda-santos <amandasf0200@gmail.com>
2020-12-12 11:11:18 -0300

    Criando rotas da aplicação

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Sobre from '../views/Sobre.vue'
+import Adote from '../views/Adote.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/sobre',
+    name: 'Sobre',
+    component: Sobre
+  },
+  {
+    path: '/adote',
+    name: 'Adote',
+    component: Adote
   }
 ]
 
---
+++ app/src/views/Sobre.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="sobre">
+    <h1>Sobre</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Sobre',
+}
+</script>
