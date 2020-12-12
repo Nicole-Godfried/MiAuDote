a9d7cbd3e84e258074ad4dade369ca833a6ce162
amanda-santos <amandasf0200@gmail.com>
2020-12-12 14:20:51 -0300

    Logo em melhor qualidade

--- app/src/components/AppFooter.vue
+++ app/src/components/AppFooter.vue
@@ -1,41 +1,28 @@
 <template>
- 
- <v-footer class="col-12" color="#00aFbb"> 
-         
-      <div class="footer">
-     <img size="30px" class="logo-footer" src="../assets/MiAuDote_Logo_02.png" alt="">
-     </div>
-     
- </v-footer>
-  
+  <v-footer class="col-12" color="#00aFbb">
+    <div class="footer">
+      <img size="30px" class="logo-footer" src="../assets/logo.svg" alt="MiAuDote" />
+    </div>
+  </v-footer>
 </template>
 
 <script>
 export default {
   name: "AppFooter",
   data: () => ({
-      icons: [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ],
-    }),
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
 };
 </script>
 
 <style scoped>
+.footer {
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+}
 
-    .footer{
-    display: flex;
-    margin: 0 auto;
-    
-    
-    }
-
-    .logo-footer{
-        
-        width: 50%;
-        
-    }
+.logo-footer {
+  width: 50%;
+}
 </style>
